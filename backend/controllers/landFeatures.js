4d4f0d1892e1ea41f874d6bfd01c08ea7cefb4ae
Lucifer <itzmegagan2@gmail.com>
2025-04-02 03:30:42 +0530

    Completed backend for the application

---
+++ backend/controllers/landFeatures.js
@@ -0,0 +1,48 @@
+const Land = require("../models/Land");
+async function uploadLand(req, res) {
+    try {
+        const {address,offering_price,description} = req.body;
+
+        const imagePath = req.files ? req.files.map(file=>file.path.replace(/\\/g,"/").replace(/^uploads\//,"")):[];
+
+        await Land.create({
+            address,
+            image:imagePath,
+            offering_price,
+            description
+        });
+        return res.status(201).json("Land uploaded successfully");
+    }
+    catch(err) {
+        console.log(err.message);
+        return res.status(501).json("Some error occured please try again later");
+    }
+}
+
+async function getLands(req,res) {
+    try {
+        const result = await Land.find();
+        const formattedResult = await result.map(land=>({
+            ...land._doc,
+            image:land.image.map(img=>`${req.protocol}://${req.get("host")}/uploads/${img}`)
+        }));
+        return res.status(201).json(formattedResult);
+    }
+    catch(err) {
+        console.log(err.message);
+        return res.status(501).json("Some error occured please try again later");
+    }
+}
+async function deleteLand(req, res) {
+    try {
+        const id = req.params.id;
+        await Land.findOneAndDelete({_id:id});
+        return res.status(201).json("Land deleted successfully");
+    }
+    catch(err) {
+        console.log(err.message);
+        return res.status(501).json("Some error occured please try again later");
+    }
+}
+
+module.exports = {uploadLand,getLands,deleteLand};
--- backend/routes/features.js
+++ backend/routes/features.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {uploadLand,getLands,deleteLand} = require("../controllers/landFeatures");
 const {handleGettingProperties,handlePostingProperties,handleDeletingProperties} = require("../controllers/features");
 const router = express.Router();
 const upload = require('../config/multerConfig');
@@ -6,6 +7,8 @@ const checkAuth = require("../middlewares/checkAuth");
 router.get("/properties",handleGettingProperties);
 router.post("/upload-property",checkAuth,upload.array("image",6),handlePostingProperties);
 router.post("/delete-property/:id",checkAuth,handleDeletingProperties);
-
+router.get("/lands-for-sale",getLands);
+router.post("/upload-land",checkAuth,upload.array("image",6),uploadLand);
+router.post("/delete-land/:id",checkAuth,deleteLand);
 
 module.exports = router;
